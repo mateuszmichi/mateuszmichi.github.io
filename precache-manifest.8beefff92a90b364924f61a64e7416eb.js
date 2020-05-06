3541b9f2c0b3e77ea590a8f185035bf4afac105b
mateuszmichi <37980373+mateuszmichi@users.noreply.github.com>
2020-05-06 21:47:05 +0200

    Initial host

---
+++ precache-manifest.8beefff92a90b364924f61a64e7416eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0a57aaee0ca3ed2a630e53ece3effe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7987753008d645e49c39",
+    "url": "/static/css/2.22ad2263.chunk.css"
+  },
+  {
+    "revision": "f2958a3cd417452eed63",
+    "url": "/static/css/main.88635177.chunk.css"
+  },
+  {
+    "revision": "7987753008d645e49c39",
+    "url": "/static/js/2.886b7215.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.886b7215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2958a3cd417452eed63",
+    "url": "/static/js/main.d7363f3c.chunk.js"
+  },
+  {
+    "revision": "79a8b3ca88fe50bf5fcb",
+    "url": "/static/js/runtime-main.1b238c34.js"
+  }
+]);
